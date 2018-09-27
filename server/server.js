14ce65d87e66645d7aed1e120ba879a23d6346e6
avinash <avinash.kumar>
2018-09-27 18:03:17 +0530

    add post user route and token methods

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -7,6 +9,7 @@ var {User} = require('./models/user');
 
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -30,28 +33,85 @@ app.get('/todos' ,(req,res) => {
   });
 });
 
-app.listen(3000 , () => {
-  console.log('Server started on 3000');
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
+});
+
+// POST /users
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    //res.send(user);
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth',token).send(user)
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
+});
+
+app.listen(port , () => {
+  console.log(`Server started on ${port}`);
 });
 
 module.exports = {app};
-
-// var newTodo = new Todo({
-//   text:'  Edit this video    '
-// });
-//
-// newTodo.save().then((doc) => {
-//   console.log('saved Todo',doc);
-// },(e) => {
-//   console.log('unable to save Todo');
-// });
-
-// var newUser = new User({
-//   email:'  avinashkr2712@gmail.com    '
-// });
-//
-// newUser.save().then((doc) => {
-//   console.log(JSON.stringify(doc,undefined,2));
-// },(e) => {
-//   console.log('unable to save User');
-// });
